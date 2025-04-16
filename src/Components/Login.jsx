a8f472ac898e4f8a18716fdbc1f19b9b5d4b79ef
FlikLik <dorantesrodriguezu@gmail.com>
2025-04-15 22:43:51 -0600

    Correción del funcionamiento de las notificaciones

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -7,39 +7,32 @@ import { useNavigate } from 'react-router-dom'
 export default function Login() {
 
     const navigate = useNavigate()
-    const [code, setCode] = useState('')
-    const [res, setRes] = useState(0)
+    const [code, setCode] = useState("")
     const [showLoader, setShowLoader] = useState(false)
-    var loading
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        loading = toast.loading('Buscando usuario...', { className: 'toast-loading' })
-        axios.post('http://localhost:3000/login', { code })
-            .then((response) => {
-                console.log('Datos recibidos', response.data.count)
-                setRes(response.data)
-                login()
+        const loading = toast.loading('Buscando usuario...', { className: 'toast-loading' })
+        axios.post('http://localhost:3000/login', { code }, { headers: { "Cache-Control": "no-cache" } })
+            .then(response => {
+                if (response.data.count === 0) {
+                    toast.update(loading, { render: 'Usuario no encontrado', type: 'error', isLoading: false, autoClose: 3000, className: 'toast-error' })
+                }
+                if (response.data.count === 1) {
+                    toast.update(loading, { render: 'Usuario encontrado', type: 'success', isLoading: false, autoClose: 3000, className: 'toast-loading' })
+                    setShowLoader(true)
+                    setTimeout(() => {
+                        navigate('/home')
+                        setShowLoader(false)
+                    }, 2000)
+                }
             })
-            .catch((error) => {
+            .catch(error => {
                 toast.update(loading, { render: 'Error al conectar', type: 'error', isLoading: false, autoClose: 3000, className: 'toast-error' })
                 console.log('Error al conectar', error)
             })
     }
 
-    const login = () => {
-        if (res === 0) {
-            toast.update(loading, { render: 'Usuario no encontrado', type: 'error', isLoading: false, autoClose: 3000, className: 'toast-error' })
-        } else {
-            toast.update(loading, { render: 'Usuario encontrado', type: 'success', isLoading: false, autoClose: 3000, className: 'toast-loading' })
-            setShowLoader(true)
-            setTimeout(() => {
-                navigate('/home')
-                setShowLoader(false)
-            }, 2000)
-
-        }
-    }
 
     return (
         <div className="is-flex is-flex-direction-column is-justify-content-center is-align-items-center is-align-content-center is-flex-wrap-wrap">
@@ -62,7 +55,12 @@ export default function Login() {
                     <br />
                     <button type='submit' className='button is-info' onClick={handleSubmit}>Login</button>
                     {
-                        showLoader && <img src="loading1.svg" alt="loading" width={10} height={10} className='loading-overlay' />
+                        showLoader &&
+                        (
+                            <div className='loading-overlay'>
+                                <img src="loading1.svg" alt="loading" className='loading-svg' />
+                            </div>
+                        )
                     }
                 </form>
             </section>
