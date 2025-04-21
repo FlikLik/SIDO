75f4d5215b2b755c968373759128037c06af11c6
FlikLik <dorantesrodriguezu@gmail.com>
2025-04-20 19:32:58 -0600

    Se lograron construir 25 preguntas de la encuesta

--- src/Backend/questions.js
+++ src/Backend/questions.js
@@ -1,7 +1,7 @@
 export const questions = [
     {
-        id: 'q1',
-        label: 'Nombre:',
+        id: 'name',
+        label: '1. Nombre:',
         type: 'text',
         placeholder: 'Nombre',
         size: 35,
@@ -9,8 +9,30 @@ export const questions = [
         icon: 'person.svg',
     },
     {
-        id: 'q2',
-        label: 'Puesto actual:',
+        id: 'age',
+        label: '2. Edad:',
+        type: 'number',
+        placeholder: 'Edad',
+        size: 3,
+        value: '',
+        min: 0,
+        max: 100,
+        icon: 'age.svg',
+    },
+    {
+        id: 'experience',
+        label: '3. Años de experiencia:',
+        type: 'number',
+        placeholder: 'Años',
+        size: 3,
+        value: '',
+        min: 0,
+        max: 40,
+        icon: 'experienceyears.svg',
+    },
+    {
+        id: 'position',
+        label: '4. Puesto actual:',
         type: 'text',
         placeholder: 'Puesto actual',
         size: 35,
@@ -18,9 +40,162 @@ export const questions = [
         icon: 'position.svg',
     },
     {
-        id: 'q3',
-        label: 'Nivel dentro de la empresa:',
+        id: 'level',
+        label: '5. Nivel dentro de la empresa:',
         type: 'radio',
         options: ["Director", "Gerente", "Supervisor", "Empleado"]
-    }
+    },
+
+    {
+        id: 'seniority',
+        label: '6. Años en la empresa:',
+        type: 'number',
+        placeholder: 'Años',
+        size: 3,
+        value: '',
+        min: 0,
+        max: 50,
+        icon: 'seniority.svg',
+    },
+    {
+        id: 'positions',
+        label: '7. Puestos ocupados en la empresa:',
+        type: 'text',
+        placeholder: 'Puestos ocupados',
+        size: 35,
+        value: '',
+        icon: 'position.svg',
+    },
+    {
+        id: 'grade',
+        label: '8. Escolaridad máxima:',
+        type: 'radio',
+        options: ["Maestría", "Universidad", "Bachillerato", "Secundaria"]
+    },
+    {
+        id: 'degree',
+        label: '9. Certificado/Título:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'courses',
+        label: '10. Cursos de actualización y/o entrenamiento:',
+        type: 'checkbox',
+        options: ["1", "2", "3", "4"]
+    },
+    {
+        id: 'positionKnowledge',
+        label: '11. ¿Conoce la descripción del puesto?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'functions',
+        label: '12. Mencione tres funciones específicas de su puesto:',
+        type: 'text',
+        placeholder: 'Mencione tres funciones',
+        size: 30,
+        value: '',
+        icon: 'function.svg',
+    },
+    {
+        id: 'mainObjectives',
+        label: '13. Indique tres objetivos a los que aporta con su rol en la empresa.',
+        type: 'text',
+        placeholder: 'Indique tres objetivos',
+        size: 30,
+        value: '',
+        icon: 'goals.svg',
+    },
+    {
+        id: 'misionKnowledge',
+        label: '14. ¿Conoce la misión o razón de ser de la empresa?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'philosophyKnowledge',
+        label: '15. ¿Conoce la filosofía de la empresa?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'structureKnowledge',
+        label: '16. ¿Conoce la estructura de la empresa (organigrama)?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'historyKnowledge',
+        label: '17. ¿Conoce la historia de la empresa?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'commitmentKnowledge',
+        label: '18. ¿Conoce que compromisos tiene la empresa en el presente/futuro?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'objectivesKnowledge',
+        label: '19. Mencione tres objetivos principales de la empresa:',
+        type: 'text',
+        placeholder: 'Mencione tres objetivos',
+        size: 30,
+        value: '',
+        icon: 'goals.svg',
+    },
+    {
+        id: 'valuesKnowledge',
+        label: '20. Indique tres valores y/o normas morales/éticas de la empresa:',
+        type: 'text',
+        placeholder: 'Indique tres valores/normas',
+        size: 30,
+        value: '',
+        icon: 'values.svg',
+    },
+    {
+        id: 'servicesKnowledge',
+        label: '21. Destaque los principales productos/servicios que ofrece la empresa:',
+        type: 'text',
+        placeholder: 'Destaque productos/servicios',
+        size: 30,
+        value: '',
+        icon: 'services.svg',
+    },
+    {
+        id: 'politicsKnowledge',
+        label: '22. ¿Conoce las políticas/procedimientos que norman la empresa?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'departmentGoalsKnowledge',
+        label: '23. ¿Conoce las metas de su departamento?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
+    {
+        id: 'personalGoals',
+        label: '24. Indique tres objetivos personales dentro de la empresa:',
+        type: 'text',
+        placeholder: 'Indique tres objetivos',
+        size: 30,
+        value: '',
+        icon: 'goals2.svg',
+    },
+    {
+        id: 'personalGoalsTime',
+        label: '24b. Indique con que frecuencia miden los objetivos personales:',
+        type: 'radio',
+        options: ["Anual", "Mensual", "Semanal", "Diaria"]
+    },
+    {
+        id: 'procedureKnowledge',
+        label: '25. ¿Conoce los procedimientos, metodologías e instructivos para realizar su trabajo?:',
+        type: 'radio',
+        options: ["Si", "No"]
+    },
 ]
--- src/Components/Survey.jsx
+++ src/Components/Survey.jsx
@@ -10,6 +10,14 @@ export default function Survey() {
         setAnswers({ ...answers, [questionId]: value })
     }
 
+    const handleCheckbox = (questionId, option) => {
+        const currentAnswers = answers[questionId] || []
+        const updateAnswers = currentAnswers.includes(option)
+            ? currentAnswers.filter((item) => item !== option)
+            : [...currentAnswers, option]
+        setAnswers({ ...answers, [questionId]: updateAnswers })
+    }
+
     return (
         <div className="is-flex gap-2 is-justify-content-center is-align-items-center is-align-content-center is-flex-wrap-wrap is-flex-direction-column">
             <h1 className={"title is-1 m-6 has-text-centered " + styles.title}>Escuesta del Diagnóstico de Competitividad Organizacional</h1>
@@ -24,7 +32,7 @@ export default function Survey() {
                                             <>
                                                 <label className="label is-size-4">{question.label}</label>
                                                 <div className='control'>
-                                                    <input className='input is-medium' type={question.type} placeholder={question.placeholder} size={question.size} value={answers[question.id]} onChange={(e) => handleChange(question.id, e.target.value)} />
+                                                    <input className='input is-medium' type={question.type} placeholder={question.placeholder} size={question.size} value={answers[question.value]} onChange={(e) => handleChange(question.id, e.target.value)} required />
                                                     <span className='icon is-medium is-left'>
                                                         <img src={question.icon} alt={question.icon} width={35} height={35} />
                                                     </span>
@@ -42,6 +50,37 @@ export default function Survey() {
                                                         question.options.map((option) => (
                                                             <label key={option} className='is-size-5 has-text-weight-bold'>
                                                                 <input type={question.type} name={question.id} value={option} checked={answers[question.id] === option} onChange={(e) => handleChange(question.id, e.target.value)} className='mr-1' />
+                                                                {option}
+                                                            </label>
+                                                        ))
+                                                    }
+                                                </div>
+                                                <br />
+                                            </>
+                                        )
+                                    }
+                                    {
+                                        question.type === 'number' && (
+                                            <div className='is-flex is-align-items-center'>
+                                                <label className="label is-size-4 is-flex-grow-1">{question.label}</label>
+                                                <div className='control'>
+                                                    <input className={'input is-medium ' + styles.input} type={question.type} placeholder={question.placeholder} size={question.size} min={question.min} max={question.max} value={answers[question.value]} onChange={(e) => handleChange(question.id, e.target.value)} required />
+                                                    <span className='icon is-medium is-left'>
+                                                        <img src={question.icon} alt={question.icon} width={35} height={35} />
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        question.type === 'checkbox' && (
+                                            <>
+                                                <label className="label is-size-4">{question.label}</label>
+                                                <div className='checkboxes grid'>
+                                                    {
+                                                        question.options.map((option) => (
+                                                            <label key={option}>
+                                                                <input className={styles.input} type={question.type} value={option} checked={(answers[question.id] || []).includes(option)} onChange={() => handleCheckbox(question.id, option)} />
                                                                 {option}
                                                             </label>
                                                         ))
