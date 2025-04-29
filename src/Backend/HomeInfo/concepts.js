a6d13bbdd6f952b66dbfe323cbf451e1ff6b5913
FlikLik <dorantesrodriguezu@gmail.com>
2025-04-28 21:28:16 -0600

    Primera parte del apartado de los conceptos completada en funcionamiento, faltan estilos

---
+++ src/Backend/HomeInfo/concepts.js
@@ -0,0 +1,32 @@
+export const ecaiConcepts = [
+    {
+        id: 1,
+        title: 'Definición',
+        content: 'Este modelo se utiliza para estructurar programas de desarrollo de competencias, asegurando que las personas adquieran los conocimientos, habilidades y actitudes necesarias para un desempeño eficiente en sus áreas laborales.',
+        list: ['Educación: Proceso formativo que brinda conocimientos y habilidades generales para el desarrollo profesional y personal.',
+            'Capacitación: Enseñanza enfocada en desarrollar habilidades específicas relacionadas con el desempeño laboral.',
+            'Adiestramiento: Entrenamiento práctico para realizar tareas concretas o mejorar el desempeño en actividades específicas.',
+            'Instrucción: Transmisión de conocimientos específicos para cumplir con procesos o procedimientos establecidos.'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Indicadores de Educación',
+        list: ['Misión de la Organización', 'Filosofía de la Organización', 'Valores de la Organización', 'Normas de la organización', 'Conocimientos de la historia de la organización', 'Conocimientos de productos y sistemas de la organización', 'Conodimientos de la estructura organizacional y sus funciones', 'Conocimiento de la estructura, objetivos, premisas y prioridades a corto y mediano plazo de la organización', 'Conocimiento del futuro de la organización', 'Seguridad del personal de contar con su trabajo', 'Razón de compromiso con la organización', 'Confianza', 'Credibilidad', 'Orden, disciplina, limpieza y seguridad', 'Competitividad', 'Reconocimiento', 'Delegación de responsabilidad', 'Identificación de valores', 'Ejemplificar con actitudes y clima', 'Integración en la solución de problemas', 'Participación con sugerencia y mejora', 'Participación con sugerencia de mejora', 'Deseo de logro y perfección', 'Aportación de conocimientos', 'Disponibilidad de aprender', 'Autocontrol', 'Conceptualización del cliente interno', 'Comunicación en la organización', 'Nivel de comunicación con el nivel superior inmediato', 'Nivel de comunicación de niveles superiores a inferiores', 'Apoyo del jefe inmediato', 'Confianza y credibilidad', 'Deseo de logro', 'Deseo de cambio e innovación para la perfección', 'Delegación de responsabilidad', 'Conocimiento de planes actuales y futuros de lo que se espera del individuo', 'Madurez de la estructura organizacional', 'Antigüedad en la empresa vs. Know How (100% con 24.7 años promedio de antigüedad)', 'Nivel académico de la organización', 'Conceptos de calidad y productividad', 'Definición de calidad', 'Definición de productividad', 'Concientización de la calidad', 'Concientización de la productividad', 'Conocimiento de quien depende la calidad', 'Nivel de búsqueda de la perfección', 'Medición de la calidad', 'Conocimiento de indicadores de calidad y productividad', 'Identificación de los efectos de la no calidad', 'El desperdicio como efecto de una organización con bajo nivel de cumplimiento', 'Conocimiento de los beneficions de la calidad']
+    },
+    {
+        id: 3,
+        title: 'Indicadores de Capacitación',
+        list: ['Detección de necesidades para un mayor desempeño', 'Desarrollo a través de la capacitación y el adiestramiento', 'Objetividad y funcionalidad del área de trabajo', 'Conocimiento de objetivos vs. funciones', 'Conocimiento de procedimientos, metodologías y hojas de instrucción', 'Conocimiento de políticas', 'Conocimiento de planes, programas, y presupuestos', 'Conocimiento de proyectos']
+    },
+    {
+        id: 4,
+        title: 'Indicadores de Adiestramiento',
+        list: ['Experiencia industrial', 'Programa de educación básico de cultura de la organización', 'Programa de sensibilización, concientización y productividad para la excelencia', 'Programa de motivación, comunicación y reconocimiento', 'Programa de clima organizacional', 'Programa de desarrollo profesional a través de la mejora', 'Programa de mejora continua y acelerada', 'Programa de desarrollo de cuadros potenciales', 'Programa de evaluación del desempeño', 'Programa de detección de necesidades de capacidad y adiestramiento', 'Programa de promoción del personal', 'Proceso integral familiar en busca de la calidad en excelencia', 'Proceso de calidad total', 'Posición competitiva de la organización', 'Participación de mercado por competidor', 'Conceptualización de ventajas', 'Factores distintivos de la organización', 'Conocimiento de competidores nacionales e internacionales', 'Mercado potencial total', 'Detección de necesidades de mercado', 'Delegación de oportunidades y amenazas', 'Detección de blancos y metas', 'Atractividad del mercado', 'Detección de fortalezas y debilidades comparativas', 'Detección de estrategias comparativas', 'Detección de programas e inversiones', 'Proyección del mercado potencial a 5 años', 'Conocimiento de liderazgo de la ogranización', 'Conocimiento de factores políticos y socioeconómicos del país']
+    },
+    {
+        id: 5,
+        title: 'Indicadores de Instrucción',
+        list: ['Políticas que dan soporte a la filosofía', 'Procedimientos e inducción de la filosofíá', 'Comunicación de objetivos', 'Objetivos generales', 'Objetivos departamentales', 'Objetivos específicos y/o metas', 'Indicadores de calidad y productividad', 'Indicadores de premisas', 'Descripción del puesto', 'Sistema de ventas y comercialización', 'Sistema de planeación estratégica competitiva', 'Sistema de detección de necesidades del cliente', 'Sistema de calidad total', 'Sistema de identificación de proyectos potenciales', 'Sistema de desarollo, control y evaluación del desperdicio', 'Sistema de reducción de costos', 'Sistema de detección, control y evaluación del desperdicio', 'Sistema de mejora continua acelerada', 'Sistema de motivación, comunicación y reconocimiento', 'Sistema de evaluación del desempeño', 'Sistema de promoción de personal', 'Sistema de desarrollo profesional a través de la mejora continua', 'Auditoría del sistema operativo de control de calidad', 'Prograam de mejora continua acelerada']
+    }
+]
--- src/Components/Home/Concepts.jsx
+++ src/Components/Home/Concepts.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react'
+import styles from '../../Styles/concepts.module.css'
+import { ecaiConcepts } from '../../Backend/HomeInfo/concepts.js'
+
+
 export default function Concepts() {
+
+    const [concepts, setConcepts] = useState([])
+
+    const handleConcepts = (concept) => {
+        if (concept.content) {
+            setConcepts({ title: concept.title, content: concept.content, list: concept.list })
+        }
+        else {
+            setConcepts({ title: concept.title, list: concept.list })
+        }
+    }
+
     return (
-        <div>
-            Concepts
+        <div className="container has-text-centered mt-6">
+            <h1 className="title is-1">Definiciones</h1>
+
+            <div className="columns has-text-black mt-3">
+                <div className="column is-one-quarter">
+                    <h1 className="title is-2">Análisis ECAI</h1>
+                    {
+                        ecaiConcepts.map((concept) => (
+                            <button className={'mb-5 ' + styles.button} key={concept.id} onClick={() => handleConcepts(concept)}>
+                                {concept.title}
+                            </button>
+                        ))
+                    }
+                </div>
+                <div className="column has-text-black p-6">
+                    <h1 className='title is-2'>Concepto</h1>
+                    <h1 className='title is-3'>{concepts.title}</h1>
+                    <div className={styles.conceptContainer}>
+                        {
+                            concepts.content &&
+                            <p className='is-size-5 has-text-justified'>{concepts.content}</p>
+
+                        }
+                        <br />
+                        <ul className={'has-text-justified ' + styles.list}>
+                            {
+                                concepts.list &&
+                                concepts.list.map((item, index) => (
+                                    <li key={index} className='is-size-5 ml-5'>{item}</li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
