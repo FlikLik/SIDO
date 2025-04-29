769c3df98597cf49e6e70ffcfdab38c428303613
FlikLik <dorantesrodriguezu@gmail.com>
2025-04-29 11:28:12 -0600

    Concepts avance 2

--- src/Backend/HomeInfo/concepts.js
+++ src/Backend/HomeInfo/concepts.js
@@ -30,3 +30,56 @@ export const ecaiConcepts = [
         list: ['Políticas que dan soporte a la filosofía', 'Procedimientos e inducción de la filosofíá', 'Comunicación de objetivos', 'Objetivos generales', 'Objetivos departamentales', 'Objetivos específicos y/o metas', 'Indicadores de calidad y productividad', 'Indicadores de premisas', 'Descripción del puesto', 'Sistema de ventas y comercialización', 'Sistema de planeación estratégica competitiva', 'Sistema de detección de necesidades del cliente', 'Sistema de calidad total', 'Sistema de identificación de proyectos potenciales', 'Sistema de desarollo, control y evaluación del desperdicio', 'Sistema de reducción de costos', 'Sistema de detección, control y evaluación del desperdicio', 'Sistema de mejora continua acelerada', 'Sistema de motivación, comunicación y reconocimiento', 'Sistema de evaluación del desempeño', 'Sistema de promoción de personal', 'Sistema de desarrollo profesional a través de la mejora continua', 'Auditoría del sistema operativo de control de calidad', 'Prograam de mejora continua acelerada']
     }
 ]
+
+export const desperdicioConcepts = [
+    {
+        id: 1,
+        title: 'Definición de porcentajes',
+        content: ['Porcentaje Obtenido: Este porcentaje refleja el nivel de cumplimiento de los indicadores establecidos para cada concepto evaluado. Un 40% , por ejemplo, indica que la organización está cumpliendo parcialmente con los estándares esperados. Un 20% , en cambio, muestra un bajo nivel de cumplimiento, indicando que la mayoría de los indicadores no se están alcanzando y que existe una necesidad urgente de intervención.', 'Porcentaje menor al 50%: Cuando el cumplimiento es del 50% o menor , esto significa que la organización está trabajando de forma limitada en los indicadores de ese concepto. Un cumplimiento bajo (-50%) refleja falta de planificación, seguimiento o recursos , y puede estar afectando el desempeño organizacional en esa área.', 'Impacto de un porcentaje bajo: Cumplimientos bajos sugieren fallas estructurales o de gestión . Por ejemplo: En Descripción de Puesto (20%) , implica que los roles y responsabilidades no están claros para los empleados, lo que genera confusión y puede afectar la productividad.', 'Cumplimiento entre 40%-55% (progresos parciales): Estos porcentajes reflejan que se están logrando desperdicios , pero no se alcanza el nivel óptimo.']
+    },
+    {
+        id: 2,
+        title: 'Misión, Filosofía, Valores, Normas y Políticas',
+        content: ['Conocimiento y Aplicación de la Misión - 100% del Personal', 'Conocimiento y Aplicación de la Filosofía - 100% del personal', 'Conocimiento y Aplicación de los Valores - 100% del Personal', 'Conocimiento y Aplicación de los Normas - 100% del Personal', 'Conocimiento y Aplicación de los Políticas - 100% del Personal', 'Conocimiento y Aplicación de los Procedimientos - 100% del Personal', 'Conocimiento de la historia de la Org. - 100% del Personal', 'Conocimiento y Aplicación de los Objetivos en el Corto y Mediano Plazo - 100% del Personal', 'Conocimiento del futuro de la Org. - 100% del Personal']
+    },
+    {
+        id: 3,
+        title: 'Descripción del puesto',
+        content: ['Actualización de la Descripción de los Puestos - 100%', 'Difusión de Descripción de Puestos - 100%', 'Aplicación de la Descripción de Puestos - 100%']
+    },
+    {
+        id: 4,
+        title: 'Proceso de planeación estratégica',
+        content: ['Elaboración del Plan Estratégico - 1 Vez por año', 'Objetivos específicos del Plan Estratégico - 100% Cumplidos', 'Documentación de cada Objetivo Específico - 100%', 'Programas de Trabajo Actualizados - 100%', 'Resultados Objetivos de Negocio - 100% Cumplidos', 'Resultados Indicadores de Calidad - 100% Cumplen Std.']
+    },
+    {
+        id: 5,
+        title: 'Lay Out (Flujos)',
+        content: ['Demoras - 0%', 'Quejas - 0%', 'Eficiencia - 100%', 'Uso de Información - 100%', 'Inventarios - 0 días, 0%']
+    },
+    {
+        id: 6,
+        title: 'Proceso de captura y análisis de información',
+        content: ['Dias de Atraso de Captura de Información - 0%', 'Uso de Información - 100%', 'Atraso en Toma de Decisiones - 0 días', 'Información Fidedigna y Veraz - 100%']
+    },
+    {
+        id: 7,
+        title: 'Plan y programa global de entrenamiento',
+        content: ['Entrenamiento por Persona - 150 horas Mínimo', 'Personal Entrenado - 100%', 'Cumplimiento del Programa de Entrenamiento - 100%', 'Rotación de Personal - 0%']
+    },
+    {
+        id: 8,
+        title: 'Planes y programas adminstrativos y operativos',
+        content: ['Objetivos - 100% Cumplidos', 'Documentación de Apoyo - 100%', 'Programas de Trabajo Actualizados - 100%', 'Elaboración de Planes y Minutas - 100% Reuniones', 'Cumplimiento de Compromisos - 100%']
+    },
+    {
+        id: 9,
+        title: 'Proyectos de mejora',
+        content: ['Objetivos - 100% Cumplidos', 'Documentación de Apoyo - 100%', 'Programas de Trabajo Actualizados - 100%', 'Elaboración de Agendas y Minutas - 100% Reuniones', 'Cumplimiento de Compromisos - 100%']
+    },
+    {
+        id: 10,
+        title: 'Solución de problemas',
+        content: ['Propuestas de Mejora Implantada - 100%', 'Personal con Propuestas de Mejora - 100%', 'Problemas Documentados - 100%', 'Cumplimiento de Compromisos - 100%']
+    }
+]
--- src/Components/Home/Concepts.jsx
+++ src/Components/Home/Concepts.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import styles from '../../Styles/concepts.module.css'
-import { ecaiConcepts } from '../../Backend/HomeInfo/concepts.js'
+import { ecaiConcepts, desperdicioConcepts } from '../../Backend/HomeInfo/concepts.js'
 
 
 export default function Concepts() {
 
     const [concepts, setConcepts] = useState([])
+    const [despConcept, setDespConcept] = useState([])
 
     const handleConcepts = (concept) => {
         if (concept.content) {
@@ -16,13 +17,17 @@ export default function Concepts() {
         }
     }
 
+    const handleDespConcepts = (concept) => {
+        setDespConcept({ title: concept.title, content: concept.content })
+    }
+
     return (
         <div className="container has-text-centered mt-6">
             <h1 className="title is-1">Definiciones</h1>
 
             <div className="columns has-text-black mt-3">
                 <div className="column is-one-quarter">
-                    <h1 className="title is-2">Análisis ECAI</h1>
+                    <h1 className="title is-3">Análisis ECAI</h1>
                     {
                         ecaiConcepts.map((concept) => (
                             <button className={'mb-5 ' + styles.button} key={concept.id} onClick={() => handleConcepts(concept)}>
@@ -31,8 +36,8 @@ export default function Concepts() {
                         ))
                     }
                 </div>
-                <div className="column has-text-black p-6">
-                    <h1 className='title is-2'>Concepto</h1>
+                <div className="column has-text-black pl-6 pr-6">
+                    <h1 className='title is-3'>Concepto</h1>
                     <h1 className='title is-3'>{concepts.title}</h1>
                     <div className={styles.conceptContainer}>
                         {
@@ -49,6 +54,35 @@ export default function Concepts() {
                                 ))
                             }
                         </ul>
+                    </div>
+                </div>
+                <br />
+            </div>
+            <div className='columns has-text-black mt-3'>
+                <div className='column is-one-quarter'>
+                    <h1 className='title is-3'>Desperdicio organizacional</h1>
+                    {
+                        desperdicioConcepts.map((concept) => (
+                            <button className={'mb-5 ' + styles.button} key={concept.id} onClick={() => handleDespConcepts(concept)}>
+                                {concept.title}
+                            </button>
+                        ))
+                    }
+                </div>
+                <div className='column has-text-black pl-6 pr-6'>
+                    <h1 className='title is-3'>Concepto</h1>
+                    <h1 className='title is-3'>{despConcept.title}</h1>
+                    <div className={styles.conceptContainer}>
+                        {
+                            despConcept.content &&
+                            <ul className={'has-text-justified ' + styles.list}>
+                                {
+                                    despConcept.content.map((item, index) => (
+                                        <li key={index} className='is-size-5 ml-5'>{item}</li>
+                                    ))
+                                }
+                            </ul>
+                        }
                     </div>
                 </div>
             </div>
